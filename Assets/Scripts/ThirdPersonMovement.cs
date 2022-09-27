b920ae57a309a21a101215a3c8af897bd27685cc
Nicholas Thiangphadung <nicholas.thiangphadung@edu.futuregames.nu>
2022-09-27 13:13:38 +0200

    Cursor Lock

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -24,6 +24,7 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         if (ActivePlayerManager.GetInstance().IsItMyTurn(playerIndex))
         {
 
