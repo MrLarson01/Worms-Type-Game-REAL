502e1adc8cab53b4132590a01f9a9c8274962cfc
MrLarson01 <dennis.dempa@hotmail.com>
2022-09-28 14:40:44 +0200

    Added ammo/animations

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -8,59 +9,101 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public float fireRate = 15f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
 
-    private void Start()
+    public Animator animator;
+
+    void Start()
     {
         playerIndex = player.GetComponent<ThirdPersonMovement>().GetIndex();
 
+        currentAmmo = maxAmmo;
 
     }
 
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
 
-   
 
     // Update is called once per frame
     void Update()
     {
+
         if (ActivePlayerManager.GetInstance().IsItMyTurn(playerIndex))
-        { 
-            if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
-
         {
-            nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
-            
-        }
+            if (isReloading)
+                return;
 
 
-
-    }
-    void Shoot ()
-    {
-
-        muzzleFlash.Play();
-        
-
-        RaycastHit hit;
-       if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-            
-            PlayerHealth target = hit.transform.GetComponent<PlayerHealth>();
-            if (target != null)
+            if (currentAmmo <= 0)
             {
-                target.TakeDamage(damage);
-               
+                StartCoroutine(Reload());
+                return;
             }
 
-            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGO, 2f);
+            if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+
+            }
+
+
+
         }
-       }
+
+        IEnumerator Reload()
+        {
+            isReloading = true;
+            Debug.Log("Reloading...");
+
+            animator.SetBool("Reloading", true);
+
+            yield return new WaitForSeconds(reloadTime - .25f);
+            animator.SetBool("Reloading", false);
+            yield return new WaitForSeconds(.25f);
+
+            currentAmmo = maxAmmo;
+            isReloading = false;
+        }
+
+
+
+        void Shoot()
+        {
+
+            muzzleFlash.Play();
+
+            currentAmmo--;
+
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+                PlayerHealth target = hit.transform.GetComponent<PlayerHealth>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+
+                }
+
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
+            }
+        }
     }
 }
