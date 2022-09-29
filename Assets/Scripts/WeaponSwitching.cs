788afd1fcc77739daf4cc3936ac0bcb27fc5f160
MrLarson01 <dennis.dempa@hotmail.com>
2022-09-29 12:48:01 +0200

    Fixed weaponswitch bug

--- Assets/Scripts/WeaponSwitching.cs
+++ Assets/Scripts/WeaponSwitching.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class WeaponSwitching : MonoBehaviour
 {
-
+    [SerializeField] private GameObject player;
+    private int playerIndex;
     public int selectedWeapon = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerIndex = player.GetComponent<ThirdPersonMovement>().GetIndex();
+
         SelectWeapon();
     }
 
@@ -15,51 +18,58 @@ public class WeaponSwitching : MonoBehaviour
     void Update()
     {
 
-        int previousSelectedWeapon = selectedWeapon;
-
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        if (ActivePlayerManager.GetInstance().IsItMyTurn(playerIndex))
         {
-            if (selectedWeapon >= transform.childCount - 1)
+
+            int previousSelectedWeapon = selectedWeapon;
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                if (selectedWeapon >= transform.childCount - 1)
+                    selectedWeapon = 0;
+                else
+                    selectedWeapon++;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                if (selectedWeapon <= 0)
+                    selectedWeapon = transform.childCount - 1;
+                else
+                    selectedWeapon--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
                 selectedWeapon = 0;
-            else
-                selectedWeapon++;
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-            if (selectedWeapon <= 0)
-                selectedWeapon = transform.childCount - 1;
-            else
-                selectedWeapon--;
-        }
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            selectedWeapon = 0;
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                selectedWeapon = 1;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            selectedWeapon = 1;
-        }
+            if (previousSelectedWeapon != selectedWeapon)
+            {
+                SelectWeapon();
+            }
 
-        if (previousSelectedWeapon != selectedWeapon)
-        {
-            SelectWeapon();
-        }
-
-    }
-
-    void SelectWeapon()
-    {
-        int i = 0;
-        foreach (Transform weapon in transform)
-        {
-            if (i == selectedWeapon)
-                weapon.gameObject.SetActive(true);
-            else
-                weapon.gameObject.SetActive(false);
-            i++;
         }
     }
 
+         void SelectWeapon()
+        {
+            int i = 0;
+            foreach (Transform weapon in transform)
+            {
+                if (i == selectedWeapon)
+                    weapon.gameObject.SetActive(true);
+                else
+                    weapon.gameObject.SetActive(false);
+                i++;
+            }
+        }
+
+
+
+    
 }
