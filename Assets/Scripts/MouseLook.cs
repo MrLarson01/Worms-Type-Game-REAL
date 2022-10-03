8785cce41ce475d4ebd2e12d683b6b7818e8e8a4
MrLarson01 <dennis.dempa@hotmail.com>
2022-10-03 17:15:25 +0200

    Added PM Script + Fixed FP Camera Issues
    Added player movement script plus fixed some first person camera issues.
    
    Hopefully we can use this in the normal game when we want to switch to first person mode.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,29 +4,21 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
-
-    public Transform playerBody;
-
     float xRotation = 0f;
-
-
-    // Start is called before the first frame update
+    public Vector2 turn;
+    public float sensitivity = 50;
+    public Vector3 deltaMove;
+    public float speed = 1;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
+        turn.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        turn.y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= turn.y;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, turn.x, 0);
     }
 }
